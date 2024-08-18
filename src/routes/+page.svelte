3969af52250b455b75903caabbda4ae7c0fb2a43
n3-rd <godwinjdayo@gmail.com>
2024-08-18 04:15:03 +0100

    Integrated Tanstack Query

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,45 +3,71 @@
 	import PlaylistCarousel from '$lib/components/layout/playlist-carousel.svelte';
 	import Track from '$lib/components/layout/track.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { IconHeartFilled } from '@tabler/icons-svelte';
-	import { convertMilliseconds } from '$lib/utils/timer';
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index';
 	import TopArtistsCarousel from '$lib/components/layout/top-artists-carousel.svelte';
-	import { calculateAverages, objectsToCommas } from '$lib/utils';
+	import { calculateAverages } from '$lib/utils';
 	import AudioAnalysis from '$lib/components/layout/audio-analysis.svelte';
+	import { createQuery } from '@tanstack/svelte-query';
 	export let data;
-	let forYou = data.forYou.playlists.items;
-	let userAlbums = data.userAlbums.items;
-	let onRepeat = data.onRepeat.items;
-	let topArtists = data.topArtists.items;
-	let onRepeatIds = data.onRepeatIds;
-	let audioAnalysis = calculateAverages(data.topAudioAnalysis.audio_features);
-	// console.log('audioAnalysis', audioAnalysis);
-	// console.log(onRepeatIds);
-	console.log(data);
+
+	const userAlbums = createQuery({
+		queryKey: ['userAlbums'],
+		queryFn: () => Promise.resolve(data.userAlbums.items),
+		initialData: data.userAlbums.items
+	});
+
+	const forYou = createQuery({
+		queryKey: ['forYou'],
+		queryFn: () => Promise.resolve(data.forYou.playlists.items),
+		initialData: data.forYou.playlists.items
+	});
+
+	const onRepeat = createQuery({
+		queryKey: ['onRepeat'],
+		queryFn: () => Promise.resolve(data.onRepeat.items),
+		initialData: data.onRepeat.items
+	});
+
+	const topArtists = createQuery({
+		queryKey: ['topArtists'],
+		queryFn: () => Promise.resolve(data.topArtists.items),
+		initialData: data.topArtists.items
+	});
+
+	const audioAnalysis = createQuery({
+		queryKey: ['audioAnalysis'],
+		queryFn: () => Promise.resolve(calculateAverages(data.topAudioAnalysis.audio_features)),
+		initialData: calculateAverages(data.topAudioAnalysis.audio_features)
+	});
+
+	console.log('userAlbums', $userAlbums.data);
+	console.log('forYou', $forYou.data);
+	console.log('onRepeat', $onRepeat.data);
+	console.log('topArtists', $topArtists.data);
+	console.log('audioAnalysis', $audioAnalysis.data);
 </script>
 
-<PlaylistCarousel playlistData={forYou} playlistTitle={data.forYou.message} />
-<AlbumCarousel albumData={userAlbums} albumTitle="Your albums" />
+<PlaylistCarousel playlistData={$forYou.data} playlistTitle="Based on your recent listening" />
+<AlbumCarousel albumData={$userAlbums.data} albumTitle="Your albums" />
 
 <div class="grid min-w-full grid-cols-3 gap-4 py-4">
 	<div class="flex max-h-[28rem] w-full flex-col gap-4 overflow-hidden">
 		<div class="text-2xl font-bold leading-snug">Your top tracks</div>
 		<ScrollArea>
 			<div class="flex h-full flex-col gap-4">
-				{#each onRepeat as track}
+				{#each $onRepeat.data as track}
 					<Track {track} />
 				{/each}
 			</div>
 		</ScrollArea>
 	</div>
 	<div class="h-full w-full rounded-2xl [&>*]:h-full [&>*]:w-full">
-		<TopArtistsCarousel artistsData={topArtists} />
+		<TopArtistsCarousel artistsData={$topArtists.data} />
 	</div>
 	<div class="w-full">
 		<div class="text-2xl font-bold leading-snug">Audio analysis</div>
 		<ScrollArea>
-			<AudioAnalysis {audioAnalysis} />
+			<AudioAnalysis audioAnalysis={$audioAnalysis.data} />
 		</ScrollArea>
 	</div>
 </div>
--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
 	import Track from '$lib/components/layout/track.svelte';
 	import { getDominantColorFromURL } from '$lib/utils/colors';
+	import { IconBrandSpotifyFilled } from '@tabler/icons-svelte';
 	import { createQuery } from '@tanstack/svelte-query';
 
 	export let data;
-	const album = data.albumInfo;
-	const albumCover = album.images[1].url;
-	const tracks = album.tracks.items;
-	const tracksCount = album.total_tracks;
-	const artist = data.artistInfo;
-	console.log('artist', artist);
 
-	console.log('playlist page data', data);
+	const album = createQuery({
+		queryKey: ['album'],
+		queryFn: () => Promise.resolve(data.albumInfo),
+		initialData: data.albumInfo
+	});
+	const artist = createQuery({
+		queryKey: ['artist with query'],
+		queryFn: () => Promise.resolve(data.artistInfo),
+		initialData: data.artistInfo
+	});
+
+	const albumCover = data.albumInfo.images[1].url;
+	const tracksCount = $album.data.total_tracks;
 
 	let albumColor = ''; // Initialize with an empty string
 
@@ -29,29 +36,18 @@
 		const color = await dominantColor(albumCover);
 		if (color) {
 			albumColor = color;
-			console.log('Dominant Color:', albumColor);
 		}
 	})();
-
-	const albumQuery = createQuery({
-		queryKey: ['album', album.id],
-		queryFn: () => Promise.resolve(album),
-		initialData: album
-	});
-
-	console.log('album with query', $albumQuery);
-
-	const artistQuery = createQuery({
-		queryKey: ['artist with query', artist.id],
-		queryFn: () => Promise.resolve(artist),
-		initialData: artist
-	});
-
-	console.log('artist with query', $artistQuery);
 </script>
 
 <main>
-	{#if albumColor}
+	{#if $album.isLoading}
+		<div class="flex min-h-screen w-full items-center justify-center">
+			<IconBrandSpotifyFilled class="h-16 w-16 animate-spin" />
+		</div>
+	{:else if $album.isError}
+		<p>Error: {$album.error.message}</p>
+	{:else if $album.isSuccess}
 		<div class="min-h-screen w-full">
 			<div
 				class="flex h-[40%] w-full flex-col gap-8 p-8 md:flex-row md:items-end md:pl-12"
@@ -60,19 +56,19 @@
 				<img
 					class="h-64 w-64 rounded-lg object-cover object-center shadow-lg md:h-72 md:w-72"
 					src={albumCover}
-					alt={$albumQuery.data.name}
+					alt={$album.data.name}
 				/>
 				<div class="flex flex-col gap-2 md:gap-3">
-					<p class="text-sm uppercase tracking-wide md:text-lg">{$albumQuery.data.album_type}</p>
-					<h1 class="text-4xl font-bold tracking-tight md:text-7xl">{$albumQuery.data.name}</h1>
+					<p class="text-sm uppercase tracking-wide md:text-lg">{$album.data.album_type}</p>
+					<h1 class="text-4xl font-bold tracking-tight md:text-7xl">{$album.data.name}</h1>
 					<div class="flex items-center gap-2">
 						<div class="flex items-center gap-2">
 							<img
-								src={$artistQuery.data.images[1].url}
-								alt={$artistQuery.data.name}
+								src={$artist.data.images[1].url}
+								alt={$artist.data.name}
 								class="h-7 w-7 rounded-full"
 							/>
-							<span class="text-sm md:text-lg">{$artistQuery.data.name}</span>
+							<span class="text-sm md:text-lg">{$artist.data.name}</span>
 						</div>
 						<div class="h-2 w-2 rounded-full bg-foreground"></div>
 						<p class="text-sm md:text-lg">{tracksCount} tracks</p>
@@ -81,14 +77,11 @@
 			</div>
 
 			<div class="rounded-t-xl px-4 py-6 md:px-8 lg:px-12">
-				{#each $albumQuery.data.tracks.items as track}
+				{#each $album.data.tracks.items as track}
 					<Track {track} />
 					<!-- {console.log(track)} -->
 				{/each}
 			</div>
 		</div>
-	{:else}
-		<!-- Placeholder while the color is loading -->
-		<div class="min-h-screen w-full bg-gray-200"></div>
 	{/if}
 </main>
